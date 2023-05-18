12e82bab426d907a7472084a2df038db5ee58f16
solidityphantom <solidityphantom@proton.me>
2023-05-17 20:19:19 -0500

    implement evenly balanced staking

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   HStack,
   Input,
@@ -6,24 +7,45 @@ import {
   InputRightElement,
   Link,
   Spinner,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
   Text,
+  Th,
+  Thead,
+  Tr,
   VStack,
 } from "@chakra-ui/react";
 import styles from "@styles/Home.module.css";
 import {
   useAccount,
   useBalance,
+  useContractRead,
   useContractWrite,
   usePrepareContractWrite,
 } from "wagmi";
 import { useState } from "react";
 import { ethers } from "ethers";
 import MultiStaker from "@data/MultiStaker.json";
+import { validators } from "@data/validators.json";
 
 function Home() {
   const address = useAccount();
   const { data: balance } = useBalance(address);
   const [amount, setAmount] = useState("0");
+  const [selectedValidators, setSelectedValidators] = useState<string[]>([]);
+
+  const handleValidatorCheck = (operator_address: string) => {
+    if (selectedValidators.includes(operator_address)) {
+      setSelectedValidators(
+        selectedValidators.filter((item) => item !== operator_address)
+      );
+    } else {
+      setSelectedValidators([...selectedValidators, operator_address]);
+    }
+  };
 
   const maxValue = balance.formatted;
 
@@ -35,13 +57,17 @@ function Home() {
     setAmount(event.target.value);
   };
 
+  const dividedAmount = selectedValidators.length
+    ? ethers.utils.parseEther(amount).div(selectedValidators.length)
+    : ethers.utils.parseEther("0");
+
   const { config: stakeConfig } = usePrepareContractWrite({
     address: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS as any,
     abi: MultiStaker.abi,
     functionName: "stakeTokens",
     args: [
-      ["evmosvaloper10t6kyy4jncvnevmgq6q2ntcy90gse3yxa7x2p4"],
-      [ethers.utils.parseEther(amount)],
+      selectedValidators,
+      Array(selectedValidators.length).fill(dividedAmount),
     ],
   });
 
@@ -76,18 +102,56 @@ function Home() {
             </InputRightElement>
           </InputGroup>
         </HStack>
+        <TableContainer height="500px" overflowY="scroll">
+          <Table variant="simple">
+            <TableCaption>Validators on tEVMOS</TableCaption>
+            <Thead>
+              <Tr>
+                <Th>Validator</Th>
+                <Th>Tokens</Th>
+                <Th>Commission</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {validators.map(
+                ({ operator_address, description, tokens, commission }) => (
+                  <Tr
+                    key={operator_address}
+                    onClick={() => handleValidatorCheck(operator_address)}
+                    className={
+                      selectedValidators.includes(operator_address)
+                        ? styles.selected
+                        : undefined
+                    }
+                  >
+                    <Td>{description.moniker}</Td>
+                    <Td>{(Number(tokens) / 1e18).toFixed(2)}</Td>
+                    <Td isNumeric>
+                      {(Number(commission.commission_rates.rate) * 100).toFixed(
+                        2
+                      )}
+                      %
+                    </Td>
+                  </Tr>
+                )
+              )}
+            </Tbody>
+          </Table>
+        </TableContainer>
+        <Box h="1rem" />
         <Button bgColor="teal" onClick={() => stake?.()}>
           {isStakeLoading ? <Spinner /> : "Stake"}
         </Button>
+        {isStakeSuccess && (
+          <VStack>
+            <Text>Staked successfully!</Text>
+            <Link href={`https://testnet.escan.live/tx/${stakeTxn.hash}`}>
+              View transaction
+            </Link>
+          </VStack>
+        )}
       </VStack>
-      {isStakeSuccess && (
-        <VStack>
-          <Text>Staked successfully!</Text>
-          <Link href={`https://testnet.escan.live/tx/${stakeTxn.hash}`}>
-            View transaction
-          </Link>
-        </VStack>
-      )}
+      <Box h="1rem" />
       <Text className={styles.bold}>
         Built with ❤️ at EVMOS Extensions Hackathon
       </Text>
---
+++ contracts/scripts/query.ts
@@ -0,0 +1,45 @@
+import { ethers } from "hardhat";
+import "@nomiclabs/hardhat-ethers";
+import fs from "fs";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+async function main() {
+  try {
+    const data = fs.readFileSync("./data/sorted_validators.json", "utf-8");
+    const validators = JSON.parse(data).validators;
+    const validatorAddr1 = validators[5].operator_address;
+    const validatorAddr2 = validators[6].operator_address;
+
+    const MultiStaker = await ethers.getContractFactory("MultiStaker");
+
+    const multiStaker = MultiStaker.attach(
+      process.env.CONTRACT_ADDRESS ?? "" // the address where your contract is deployed
+    );
+
+    // Get delegation
+    const delegation1 = await multiStaker.getDelegation(validatorAddr1);
+    const delegation2 = await multiStaker.getDelegation(validatorAddr2);
+    console.log(
+      "Delegation 1: ",
+      ethers.utils.formatUnits(delegation1[1][1], 18)
+    );
+    console.log(
+      "Delegation 2: ",
+      ethers.utils.formatUnits(delegation2[1][1], 18)
+    );
+
+    // Get delegators
+    const delegators = await multiStaker.getDelegatorValidators();
+    console.log("Delegators: ", delegators);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
